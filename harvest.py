c32ca051f17775687054364b07bca0b4ebdc5170
Kelli Budd <buddkelli1@gmail.com>
2020-05-08 02:37:04 +0000

    Create Melon class, add melon harvesting attributes, make subclass of melons for each harvested melon, sellability report and sellabale check in progress

--- harvest.py
+++ harvest.py
@@ -6,8 +6,8 @@
 class MelonType(object):
     """A species of melon at a melon farm."""
 
-    def __init__(self, code, name, first_harvest, color, is_seedless, is_bestseller, 
-                 ):
+    def __init__(self, code, name, first_harvest, color, is_seedless, 
+                is_bestseller):
         """Initialize a melon."""
     
         self.code = code
@@ -54,28 +54,32 @@ def make_melon_types():
     yw.add_pairing('ice cream')
     all_melon_types.append(yw)
 
-
     return all_melon_types
 
-def print_pairing_info(melons):
+melon_types = make_melon_types()
+
+def print_pairing_info(melon_types):
     """Prints information about each melon type's pairings."""
 
-    for melon in melons:
+    for melon in melon_types:
         pairs = ', '.join(melon.pairings)
         print(f'{melon.name} pairs with {pairs}')
 
-def make_melon_type_lookup(melons):
+print_pairing_info(melon_types)
+
+def make_melon_type_lookup(melon_types):
     """Takes a list of MelonTypes and returns a dictionary of melon type by code."""
 
     melon_dict = {}
 
-    for melon in melons:
+    for melon in melon_types:
         key = melon.code
         value = melon
         melon_dict[key] = value
 
     return melon_dict
 
+melons_by_id = make_melon_type_lookup(melon_types)
 
 ############
 # Part 2   #
@@ -84,18 +88,75 @@ def make_melon_type_lookup(melons):
 class Melon(object):
     """A melon in a melon harvest."""
 
-    # Fill in the rest
-    # Needs __init__ and is_sellable methods
+    def __init__(self, melon_type, shape_rating, color_rating, 
+                harvest_location, harvested_by):
+        """Initialize a melon."""
+    
+        self.melon_type = melon_type
+        self.shape_rating = shape_rating
+        self.color_rating = color_rating
+        self.harvest_location = harvest_location
+        self.harvested_by = harvested_by
 
-def make_melons(melon_types):
+    def is_sellable(melon):
+        
+        if melon.shape_rating > 5 and melon.color_rating > 5 and melon.harvest_location != 'Field 3':
+            return True
+        else:
+            return False
+
+    is_sellable(melon)
+
+def make_melons(melons_by_id):
     """Returns a list of Melon objects."""
 
-    # Fill in the rest
+    #need to define is_sellable
 
-def get_sellability_report(melons):
+    all_harvested_melons = []
+
+    melon1 = Melon(melons_by_id['yw'],8,7,'Field 2','Sheila')
+    all_harvested_melons.append(melon1)
+
+    melon2 = Melon(melons_by_id['yw'],3,4,'Field 2','Sheila')
+    all_harvested_melons.append(melon2)
+
+    melon3 = Melon(melons_by_id['yw'],9,8,'Field 3','Sheila')
+    all_harvested_melons.append(melon3)
+
+    melon4 = Melon(melons_by_id['cas'],10,6,'Field 35','Sheila')
+    all_harvested_melons.append(melon4)
+
+    melon5 = Melon(melons_by_id['cren'],8,9,'Field 35','Michael')
+    all_harvested_melons.append(melon5)
+
+    melon6 = Melon(melons_by_id['cren'],8,2,'Field 35','Michael')
+    all_harvested_melons.append(melon6)
+
+    melon7 = Melon(melons_by_id['cren'],2,3,'Field 4','Michael')
+    all_harvested_melons.append(melon7)
+
+    melon8 = Melon(melons_by_id['musk'],6,7,'Field 4','Michael')
+    all_harvested_melons.append(melon8)
+
+    melon9 = Melon(melons_by_id['yw'],7,10,'Field 3','Sheila')
+    all_harvested_melons.append(melon9)
+
+    return all_harvested_melons
+
+harvested_melons = make_melons(melons_by_id) 
+
+
+def get_sellability_report(harvested_melons):
     """Given a list of melon object, prints whether each one is sellable."""
 
-    # Fill in the rest 
+    for melon in harvested_melons:
+        if is_sellable(melon) == True:
+            sellable = '(CAN BE SOLD)'
+        else:
+            sellable = '(NOT SELLABLE)'
 
+        print(f'Harvested by {melon.harvested_by} from {harvest_location} {sellable}')
+
+get_sellability_report(harvested_melons)
 
 
